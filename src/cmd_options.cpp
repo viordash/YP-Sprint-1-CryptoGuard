43bace52d767ba677accdb85b084a8e133c796e3
Vitalii Orazov <viordash@yandex.ru>
2025-07-04 20:44:37 +0000

    cmd_options: add requried options

--- src/cmd_options.cpp
+++ src/cmd_options.cpp
@@ -1,8 +1,17 @@
 #include "cmd_options.h"
 
 namespace CryptoGuard {
+namespace po = boost::program_options;
 
-ProgramOptions::ProgramOptions() : desc_("Allowed options") {}
+ProgramOptions::ProgramOptions() : desc_("Allowed options") {
+    desc_.add_options()                                                                         //
+        ("help,h", "список доступных опций")                                                    //
+        ("command,c", po::value<int>()->required(), "команда encrypt, decrypt или checksum")    //
+        ("input,i", po::value<std::string>(&inputFile_)->required(), "путь до входного файла")  //
+        ("output,o", po::value<std::string>(&outputFile_)->required(),
+         "путь до файла, в котором будет сохранён результат")  //
+        ("password,p", po::value<std::string>(&password_)->required(), "пароль для шифрования и дешифрования");
+}
 
 ProgramOptions::~ProgramOptions() = default;
 
