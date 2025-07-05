550606fb78e0ec59c8da4e1b19e75db66da8abb3
Vitalii Orazov <viordash@yandex.ru>
2025-07-05 15:10:44 +0000

    main: remove sample (cont. 8e80b44b7d7010053be785d1c55da9a72ce03e70)

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,11 @@
 #include "cmd_options.h"
 #include "crypto_guard_ctx.h"
 #include "handled_exception.h"
-#include <array>
 #include <iostream>
 #include <openssl/evp.h>
 #include <print>
 #include <stdexcept>
 
-struct AesCipherParams {
-    static const size_t KEY_SIZE = 32;             // AES-256 key size
-    static const size_t IV_SIZE = 16;              // AES block size (IV length)
-    const EVP_CIPHER *cipher = EVP_aes_256_cbc();  // Cipher algorithm
-
-    int encrypt;                              // 1 for encryption, 0 for decryption
-    std::array<unsigned char, KEY_SIZE> key;  // Encryption key
-    std::array<unsigned char, IV_SIZE> iv;    // Initialization vector
-};
-
-AesCipherParams CreateChiperParamsFromPassword(std::string_view password) {
-    AesCipherParams params;
-    constexpr std::array<unsigned char, 8> salt = {'1', '2', '3', '4', '5', '6', '7', '8'};
-
-    int result = EVP_BytesToKey(params.cipher, EVP_sha256(), salt.data(),
-                                reinterpret_cast<const unsigned char *>(password.data()), password.size(), 1,
-                                params.key.data(), params.iv.data());
-
-    if (result == 0) {
-        throw std::runtime_error{"Failed to create a key from password"};
-    }
-
-    return params;
-}
 
 int main(int argc, char *argv[]) {
     try {
