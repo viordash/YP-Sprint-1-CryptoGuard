e208b0d4c6a5fb74cbfdda9a80ba8af6acc0f890
Vitalii Orazov <viordash@yandex.ru>
2025-07-05 15:59:58 +0000

    CryptoGuardCtx::Impl: wip. create class crypto-struct

--- src/crypto_guard_ctx.cpp
+++ src/crypto_guard_ctx.cpp
@@ -1,10 +1,40 @@
 #include "crypto_guard_ctx.h"
+#include <array>
 #include <openssl/evp.h>
 
 namespace CryptoGuard {
 
 class CryptoGuardCtx::Impl {
+private:
+    struct AesCipherParams {
+        static const size_t KEY_SIZE = 32;             // AES-256 key size
+        static const size_t IV_SIZE = 16;              // AES block size (IV length)
+        const EVP_CIPHER *cipher = EVP_aes_256_cbc();  // Cipher algorithm
+
+        int encrypt;                              // 1 for encryption, 0 for decryption
+        std::array<unsigned char, KEY_SIZE> key;  // Encryption key
+        std::array<unsigned char, IV_SIZE> iv;    // Initialization vector
+    };
+
+    AesCipherParams CreateChiperParamsFromPassword(std::string_view password) {
+        AesCipherParams params;
+        constexpr std::array<unsigned char, 8> salt = {'1', '2', '3', '4', '5', '6', '7', '8'};
+
+        int result = EVP_BytesToKey(params.cipher, EVP_sha256(), salt.data(),
+                                    reinterpret_cast<const unsigned char *>(password.data()), password.size(), 1,
+                                    params.key.data(), params.iv.data());
+
+        if (result == 0) {
+            throw std::runtime_error{"Failed to create a key from password"};
+        }
+
+        return params;
+    }
+
 public:
+    Impl() { OpenSSL_add_all_algorithms(); }
+    ~Impl() { EVP_cleanup(); }
+
     void EncryptFile(std::iostream &inStream, std::iostream &outStream, std::string_view password) {}
 
     void DecryptFile(std::iostream &inStream, std::iostream &outStream, std::string_view password) {}
